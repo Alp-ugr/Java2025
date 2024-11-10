110908d061d9e5ab3fbfa4e49b98e3b9918cb4cb
Alp-ugr <alpugurguler@gmail.com>
2024-11-10 08:30:35 +0100

    if else

---
+++ java2025/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args){
+        int sayi1 = 13;
+        int sayi2 = 61;
+        if (sayi1<sayi2) {
+            System.out.println("en kucuk sayi:" + sayi1);
+        }else{
+            System.out.println("en kucuk sayi:" + sayi2);
+        }
+    }
+}
+
+
+
